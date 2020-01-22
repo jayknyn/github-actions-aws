84c9d81c8ebcff07f9e01689d8380fccf6f993dd
Jay Kannaiyan <jay.kannaiyan@hotschedules.com>
2020-01-22 16:18:47 -0600

    App is running, serving built file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.listen(3000, (err, res) => {
+  if (err) {
+    console.log('server error: ', err);
+  } else {
+    console.log('Server running in aisle 3000');
+  }
+});
+// app.use(express.json({ urlencoded: true }));
+app.use(express.static('dist'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/dist');
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
