81867a66368c28244ac59671f2ba46e89950a746
Jay Kannaiyan <jay.kannaiyan@hotschedules.com>
2020-01-23 15:36:26 -0600

    adding terraform to workflow, provision and push to s3

---
+++ demo.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "[jk-remote-state]"
+    key    = "[jk-demo-app]"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "[jk-remote-state]"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::[jk-remote-state]/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+  }
+}
