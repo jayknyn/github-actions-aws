9538559f41d21c9e7f3d7185a691740cf1c2d50c
Jay Kannaiyan <jay.kannaiyan@hotschedules.com>
2020-01-27 12:21:47 -0600

    updating policy syntax

--- main.tf
+++ main.tf
@@ -23,21 +23,15 @@ resource "aws_s3_bucket" "b" {
   policy = <<POLICY
   {
     "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::jibhi-test-bucket/*"
-            ]
-        }
-    ]
-}
-POLICY
+    "Statement": {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::jibhi-test-bucket/*"
+    }
+  }
+  POLICY
 
   website {
     index_document = "index.html"
