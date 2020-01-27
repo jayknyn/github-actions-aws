d61217c28fd8454a7ac17834188ecc57bfa37fea
Jay Kannaiyan <jay.kannaiyan@hotschedules.com>
2020-01-27 14:15:00 -0600

    removing s3 policy for now

--- main.tf
+++ main.tf
@@ -20,23 +20,23 @@ resource "aws_s3_bucket" "b" {
     enabled = true
   }
 
-  policy = <<EOF
-  {
-  "Id": "Policy1580150172903",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1580150156614",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::jibhi-test-bucket/*",
-      "Principal": "*"
-    }
-  ]
-}
-EOF
+  #   policy = <<EOF
+  #   {
+  #   "Id": "Policy1580150172903",
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #     {
+  #       "Sid": "Stmt1580150156614",
+  #       "Action": [
+  #         "s3:GetObject"
+  #       ],
+  #       "Effect": "Allow",
+  #       "Resource": "arn:aws:s3:::jibhi-test-bucket/*",
+  #       "Principal": "*"
+  #     }
+  #   ]
+  # }
+  # EOF
 
   website {
     index_document = "index.html"
